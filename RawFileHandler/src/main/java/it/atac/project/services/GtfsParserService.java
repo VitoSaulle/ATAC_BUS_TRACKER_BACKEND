5367de69c31548947dec6e6395a7d5f7f5260811
VitoSaulle <vitosaulle123@gmail.com>
2023-07-10 17:51:36 +0200

    Formatted GtfsParserService

--- RawFileHandler/src/main/java/it/atac/project/services/GtfsParserService.java
+++ RawFileHandler/src/main/java/it/atac/project/services/GtfsParserService.java
@@ -5,7 +5,6 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -25,18 +24,19 @@ public class GtfsParserService {
 //	@Autowired
 //	private ParsedFileTemplateHandlerService parsedFileTemplateHandlerService;
 
+	@Autowired
+	private AtacGtfsDataDocumentRepository atacGtfsDataDocumentRepository;
+
 	@KafkaListener(topics = KafkaTopics.RAW_FILE_TOPIC, groupId = KafkaGroupIds.ATAC_QUEUE)
 	public void parseGtfsFile(byte[] gtfsFile) {
-		
-		log.info("Arrived Message");
-		
+
 		List<AtacGtfsData> gtfsDataList;
 		try {
 
 			gtfsDataList = FeedMessage.parseFrom(gtfsFile).getEntityList().stream()
 					// .filter(entity -> entity.hasTripUpdate())
 					.map(entity -> {
-						log.info("Entity arrived: {}",entity.toString());
+						log.info("Entity arrived: {}", entity.toString());
 						var gtfsData = new AtacGtfsData();
 
 						gtfsData.setRouteId(entity.getVehicle().getTrip().getRouteId());
@@ -54,36 +54,23 @@ public class GtfsParserService {
 			log.error("Error while parsing gtfs file: {}", e.getMessage());
 			return;
 		}
-//
+
 //		// Do further processing with the parsed GTFS data
 		processParsedGtfsData(gtfsDataList);
 	}
 
-	@Autowired
-	private AtacGtfsDataDocumentRepository atacGtfsDataDocumentRepository;
-
 	private void processParsedGtfsData(List<AtacGtfsData> gtfsDataList) {
-	
-		log.info("total data legth: {}", gtfsDataList.size());
 
-		//gtfsDataList.forEach(data -> log.info("{}", data));
-		
-		log.info("will now begin the saving");
 		try {
 			gtfsDataList.forEach(data -> {
 				var doc = new AtacGtfsDataDocument();
 				BeanUtils.copyProperties(data, doc);
-				
-				log.info("Will now try to save {}", doc.toString());
-				
-				var saved = atacGtfsDataDocumentRepository.save(doc);
-				
-				log.info("Saved {}", saved.toString());
+
+				atacGtfsDataDocumentRepository.save(doc);
 			});
 		} catch (Exception e) {
 			log.error("Error while saving on DB... {}", e.getMessage());
 		}
-
 	}
 
 }
